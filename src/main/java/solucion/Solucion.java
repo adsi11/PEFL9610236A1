d311b904e84717dd4ca8880c5bb5bb4b52f78ed3
Fernando Peña <adsi.itam.11@gmail.com>
2018-04-20 08:03:40 -0500

    Se separan clases, agrega javadoc y método pnr

--- src/main/java/solucion/Solucion.java
+++ src/main/java/solucion/Solucion.java
@@ -1,44 +1,6 @@
 package solucion;
 
 public class Solucion {
-    class Node {
-        String data;
-        Node next;
-        
-        Node(String data) {
-            this.data = data;
-        }
-        
-        Node gus() {
-            if(next == null) return this;
-            Node otro = next;
-            next      = null;
-            Node tavo = otro.gus();
-            otro.next = this;
-            return tavo;
-        }
-      
-        /**
-         * Imprime en la consola la secuencia de Nodos que 
-         * componen a esta lista. Por ejemplo, para la lista
-         * que devuelve el método "build", la invocación de
-         * este método escribe en la consola: 
-         * 
-         * A-->B-->C-->D-->E-->F-->
-         * 
-         * Lo anterior sería lo que se visualiza en la consola 
-         * justo después de ejecutar las siguintes dos lineas:
-         * 
-         *         Node a = build();
-         *         a.prn();
-         *         
-         */
-        void prn() {
-            // Este método requiere sólo de 3 a 7 lineas de código para funcionar correctamente
-        }
-        
-    }// ends Node class
-    
     /**
      * COMENTARIO INCORRECTO:
      * 
